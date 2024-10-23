d1cadc98eddf25dbac0c0ea28bfe65275eb1ad49
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2024-10-24 02:25:21 +0600

    v3.0.10

--- app.py
+++ app.py
@@ -42,18 +42,25 @@ def get_binance_btc_price():
         return None
 
 
+proxies = {
+    "http": "http://43.134.68.153:3128",
+    "https": "http://43.134.68.153:3128",
+}
+
+
 @app.route("/test-binance")
 def test_binance():
     print("Fetching price...")
     response = requests.get(
         "https://fapi.binance.com/fapi/v1/ticker/price?symbol=BTCUSDT",
         timeout=20,
+        proxies=proxies,
         verify=False,
     )
     data = response.json()
     print(f"data: {data}")
 
-    return f"BTC price: {data}"
+    return f"BTC price: {data} | {response}"
 
 
 def format_message(action, price, take_profit, margin_percent):
